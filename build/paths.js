95174a6e19c00c9869d7204871b0bef6ca360924
Dénes Harmath <harmathdenes@gmail.com>
2015-09-19 00:29:23 +0200

    Move elm-package.json to root directory for better tooling support

--- build/paths.js
+++ build/paths.js
@@ -1,12 +1,11 @@
 var path = require('path');
 
-exports.build = path.dirname(process.argv[1]);
-exports.elmPackage = path.join(exports.build, 'elm-package.json');
+exports.root = path.join(path.dirname(process.argv[1]), '..');
+
+exports.elmPackage = path.join(exports.root, 'elm-package.json');
 var elmPackageContents = require(exports.elmPackage);
 
-exports.root = path.join(exports.build, '..');
-
-exports.sourceElm = path.join(exports.build, elmPackageContents['source-directories'][0]);
+exports.sourceElm = path.join(exports.root, elmPackageContents['source-directories'][0]);
 exports.rootModule = path.join(exports.sourceElm, elmPackageContents['exposed-modules'][0]);
 
 exports.target = path.join(exports.root, 'target');
